9e72da11c4c84d4653455b9e6fa7c59c3c2584f2
Rylan <rylanoc@gmail.com>
2018-06-01 20:10:36 -0400

    ES6-ify code

--- src/components/Challenges.vue
+++ src/components/Challenges.vue
@@ -4,7 +4,7 @@
     <b-card-group deck class="mb-3">
         <h2>Web</h2>
         <challenge-card
-        v-for="challenge in web_challenges" :key="challenge.points"
+        v-for="challenge in webChallenges" :key="challenge.points"
         v-bind:points="challenge.points"
         v-bind:catagory="challenge.catagory"
         v-bind:name="challenge.name"
@@ -18,34 +18,46 @@
 
 <script>
 import ChallengeCard from './ChallengeCard.vue'
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/'
+var webChallenges
+var shellChallenges
+var networkingChallenges
+var cryptoChallenges
+var reverseEngineeringChallenges
+var pwningChallenges
+var reconChallenges
+var steganographyChallenges
+var forensicsChallenges
+var csawChallenges
+var miscChallenges
+var physicalChallenges
 
 // returns all elements in a specified collection
-function dump_challenge_type(query_string){
-    dbo.collection(query_string).find({}).toArray(function(err, result) {
-    if (err) throw err;
-    var ret_val = result;
-    db.close();
-  });
-  return ret_val
+function dumpChallengeType (queryString, dbo, db) {
+  dbo.collection(queryString).find({}).toArray(function (err, result) {
+    if (err) throw err
+    db.close()
+    return result
+  })
 }
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  web_challenges = dump_challenge_type("web")
-  shell_challenges = dump_challenge_type("shell")
-  networking_challenges = dump_challenge_type("networking")
-  crypto_challenges = dump_challenge_type("crypto")
-  reverse_engineering_challenges = dump_challenge_type("reverse_engineering")
-  pwning_challenges = dump_challenge_type("pwning")
-  recon_challenges = dump_challenge_type("recon")
-  steganography_challenges = dump_challenge_type("steganography")
-  forensics_challenges = dump_challenge_type("forensics")
-  csaw_challenges = dump_challenge_type("csaw")
-  misc_challenges = dump_challenge_type("misc")
-  physical_challenges = dump_challenge_type("physical")
-}); 
+MongoClient.connect(url, function (err, db) {
+  var dbo = db.db('challenges')
+  if (err) throw err
+  webChallenges = dumpChallengeType('web', dbo, db)
+  shellChallenges = dumpChallengeType('shell', dbo, db)
+  networkingChallenges = dumpChallengeType('networking', dbo, db)
+  cryptoChallenges = dumpChallengeType('crypto', dbo, db)
+  reverseEngineeringChallenges = dumpChallengeType('reverse_engineering', dbo, db)
+  pwningChallenges = dumpChallengeType('pwning', dbo, db)
+  reconChallenges = dumpChallengeType('recon', dbo, db)
+  steganographyChallenges = dumpChallengeType('steganography', dbo, db)
+  forensicsChallenges = dumpChallengeType('forensics', dbo, db)
+  csawChallenges = dumpChallengeType('csaw', dbo, db)
+  miscChallenges = dumpChallengeType('misc', dbo, db)
+  physicalChallenges = dumpChallengeType('physical', dbo, db)
+})
 
 export default {
   name: 'challenges',
@@ -54,18 +66,18 @@ export default {
   },
   data () {
     return {
-      web_challenges,
-      shell_challenges,
-      networking_challenges,
-      crypto_challenges,
-      reverse_engineering_challenges,
-      pwning_challenges,
-      recon_challenges,
-      steganography_challenges,
-      forensics_challenges,
-      csaw_challenges,
-      misc_challenges,
-      physical_challenges
+      webChallenges,
+      shellChallenges,
+      networkingChallenges,
+      cryptoChallenges,
+      reverseEngineeringChallenges,
+      pwningChallenges,
+      reconChallenges,
+      steganographyChallenges,
+      forensicsChallenges,
+      csawChallenges,
+      miscChallenges,
+      physicalChallenges
     }
   }
 }
